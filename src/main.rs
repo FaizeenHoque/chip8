0cd26a8b6e2d6577783f1b925a664d3831f9928f
Faizeen Hoque <haquefaizeen11@gmail.com>
2026-07-04 00:58:08 +0600

    Added minifb to project, Start by creating the Cpu struct and a function to load the ROM, Proper commenting is also done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use minifb::{self, Key::P, Window, WindowOptions};
+
+const CHIP8_WIDTH: usize = 64;
+const CHIP8_HEIGHT: usize = 32;
+
+const SCALE: usize = 20;
+
+const WINDOW_WIDTH: usize = CHIP8_WIDTH * SCALE;
+const WINDOW_HEIGHT: usize = CHIP8_HEIGHT * SCALE;
+
 struct Cpu {
     pub registers: [u8; 16], // V0 through VF (VF is often used as a flag)
     pub index: u16, // I register — usually holds a memory address
@@ -6,7 +16,11 @@ struct Cpu {
     pub sp: u8, // stack pointer — index into the stack array
     pub delay_timer: u8,
     pub sound_timer: u8,
-    pub memory: [u8; 4096]
+    pub memory: [u8; 4096],
+
+
+    // 64x32 pixel display
+    pub display: [[bool; 64]; 32],
 }
 
 impl Cpu {
@@ -20,6 +34,7 @@ impl Cpu {
             delay_timer: 0,
             sound_timer: 0,
             memory: [0; 4096],
+            display: [[false; 64]; 32],
         }
     }
 
@@ -70,25 +85,36 @@ impl Cpu {
         let nnn = opcode & 0x0FFF;      
 
         match opcode & 0xF000 {
-            0x0000 => { 
+            0x0000 => {  // CLS
                 match opcode {
                     0x00E0 => {
                         // clear screen
+                        for row in &mut self.display {
+                            row.fill(false);
+                        }
                     }
 
                     0x00EE => {
                         // return
+                        todo!()
                     }
 
                     _ => {
                         unimplemented!("Opcode {:04X}", opcode)
                     }
                 }
-             } // CLS
+             } 
             0x1000 => {  self.pc = nnn } // JP addr
+            0x2000 => { todo!() }
+            0x3000 => { todo!() }
+            0x4000 => { todo!() }
+            0x5000 => { todo!() }
             0x6000 => {  self.registers[vx] = kk; } // LD Vx, byte
             0x7000 => {  self.registers[vx] = self.registers[vx].wrapping_add(kk) } // ADD Vx, byte
+            0x9000 => { todo!() }
             0xA000 => {  self.index = nnn } //  LD I, addr
+            0xD000 => { todo!() }
+            0xF000 => { todo!() }
             _ => { unimplemented!("Opcode {:04X}", opcode); }
         }
         
@@ -96,15 +122,60 @@ impl Cpu {
 }
 
 fn main() {
-    let rom_bytes = std::fs::read("roms/Puzzle.ch8").expect("Failed to read ROM");
+    // get rom from file and put it in rom_bytes
+    let rom_bytes = std::fs::read("roms/test.ch8").expect("Failed to read ROM");
+    // create a new cpu
     let mut cpu = Cpu::new();
+    // load the rom using the rom_bytes var
     cpu.load_rom(&rom_bytes);
 
-    loop {
+    // crate a buffer, essentially a sheet of pixels
+    let mut buffer: Vec<u32> = vec![0; WINDOW_WIDTH * WINDOW_HEIGHT];
+
+    // Create a new window.. hahahahah!
+    let mut window = Window::new(
+        "CHIP-8 Emulator",
+        WINDOW_WIDTH,
+        WINDOW_HEIGHT,
+        WindowOptions::default(),
+    )
+    .expect("Failed to create window");
+
+    // You cant be that dumb.. this is self explanatory 
+    window.set_target_fps(60);
+
+    // close window if ESCAPE key is pressed
+    while window.is_open() && !window.is_key_down(minifb::Key::Escape) {
+        // fetch instructs from rom and place them into opcode var
         let opcode = cpu.fetch();
-        // println!("{:04X}", opcode);
-        // decode + execute 
+        // execute opcodes
         cpu.execute(opcode);
-                    
+        
+        for y in 0..CHIP8_HEIGHT { 
+            for x in 0..CHIP8_WIDTH {
+                let color = if cpu.display[y][x] {
+                    0xFFFFFFFF
+                } else {
+                    0x00000000
+                };
+
+                // scale pixels according to the window 
+                for dy in 0..SCALE {
+                    for dx in 0..SCALE {
+                        let px = x * SCALE + dx;
+                        let py = y * SCALE + dy;
+
+                        buffer[py * WINDOW_WIDTH + px] = color;
+                    }
+                }
+            }
+        }
+
+        // update window with buffer
+        window
+            .update_with_buffer(&buffer, WINDOW_WIDTH, WINDOW_HEIGHT)
+            .unwrap();
     }
+
+
 }
